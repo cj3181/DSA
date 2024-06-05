b83c2930af3d9b90fa70ef8e17bbf924e4bb35b4
Christo Paul <christopaul@192.168.1.7>
2024-06-05 19:53:44 +0530

    first commit

---
+++ naya.java
@@ -0,0 +1,14 @@
+public class naya{
+    public static void main(String args[]){
+        // Scanner sc = new Scanner(System.in);
+        // int a = sc.nextInt();
+        // int b = sc.nextInt();
+        // /int sum = a+b;
+        // System.out.print("The Sum is: "+ sum);     
+        float c = 45.78f;
+        System.out.println(++c);
+        System.out.println(c);
+
+
+    }
+}
